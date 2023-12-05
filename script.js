540bf66c5d886d7916c023e6fa0f48e168f77e12
ArjunB126 <arjunbajare86@gmail.com>
2023-12-05 22:31:35 +0530

    New_Commit

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const notes = JSON.parse(localStorage.getItem("notes"));
 const updateLocalStorage = () => {
     const notesText = document.querySelectorAll("textarea");
     const notes = [];
+    notesText.forEach((note) => notes.push(note.value));
+    localStorage.setItem("notes", JSON.stringify(notes));
+  };
